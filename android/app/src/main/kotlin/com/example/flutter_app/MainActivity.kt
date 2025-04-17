2149cdccb7c7de11edb218cc72610d36da5a2e76
Sujith <sujithks.ks7@gmail.com>
2025-04-17 17:53:37 +0530

    Manifest commit

--- android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.flutter_app
 
-import io.flutter.embedding.android.FlutterActivity
-
+import io.flutter.embedding.android.FlutterActivity;
 class MainActivity: FlutterActivity() {
 }
